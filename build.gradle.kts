8419dbc9751fca20170b5c03a8962ff68f3f0b6b
Sir Wabbit <559086+sir-wabbit@users.noreply.github.com>
2025-01-18 23:38:33 -0500

    Update ASM library dependencies to version 9.7.1
    Bump org.ow2.asm:asm, asm-tree, asm-util, and asm-analysis from version 9.4 to 9.7.1.
    
    Semver Impact: MINOR

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,10 +38,10 @@ dependencies {
 
     testImplementation(kotlin("test"))
 
-    implementation("org.ow2.asm:asm:9.4")
-    implementation("org.ow2.asm:asm-tree:9.4")
-    implementation("org.ow2.asm:asm-util:9.4")
-    implementation("org.ow2.asm:asm-analysis:9.4")
+    implementation("org.ow2.asm:asm:9.7.1")
+    implementation("org.ow2.asm:asm-tree:9.7.1")
+    implementation("org.ow2.asm:asm-util:9.7.1")
+    implementation("org.ow2.asm:asm-analysis:9.7.1")
 }
 
 java {
